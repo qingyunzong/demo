b9e980ae633ca8cf21af5f427675a32fc816c382
txx2009 <917275527@qq.com>
2016-10-26 22:23:32 +0800

    青云宗主

--- qyDemo/src/com/qingyunzong/demo/Demo.java
+++ qyDemo/src/com/qingyunzong/demo/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("WE ARE NIUBILITY!!!test");
 		System.out.println("WE ARE NIUBILITY!!!sunshanpeng");
+		System.out.println("WE ARE NIUBILITY!!!青云宗主");
 	}
 }
