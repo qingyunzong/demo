bcc57265a56bbf4f988b0f641d1a65341d5dcaf1
sunshanpeng <sunshanpeng@outlook.com>
2016-10-26 22:07:56 +0800

    apeng

--- qyDemo/src/com/qingyunzong/demo/Demo.java
+++ qyDemo/src/com/qingyunzong/demo/Demo.java
@@ -3,5 +3,6 @@ package com.qingyunzong.demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("WE ARE NIUBILITY!!!test");
+		System.out.println("WE ARE NIUBILITY!!!sunshanpeng");
 	}
 }
