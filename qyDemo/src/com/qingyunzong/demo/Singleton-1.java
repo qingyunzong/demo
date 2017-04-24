46ead12dd8e2fe9ec87eaa329bc4054f73c2adc3
delin <zdl0929@gmail.com>
2017-04-24 16:45:45 +0800

    单例模式有五种写法：懒汉、饿汉、双重检验锁、静态内部类、枚举。

---
+++ qyDemo/src/com/qingyunzong/demo/Singleton-1.java
@@ -0,0 +1,12 @@
+//懒加载模式，但是却存在致命的问题。当有多个线程并行调用 getInstance() 的时候，就会创建多个实例。也就是说在多线程下不能正常工作。
+public class Singleton {
+    private static Singleton instance;
+    private Singleton (){}
+
+    public static Singleton getInstance() {
+     if (instance == null) {
+         instance = new Singleton();
+     }
+     return instance;
+    }
+}
---
+++ qyDemo/src/com/qingyunzong/demo/Singleton-6.java
@@ -0,0 +1,7 @@
+//枚举 Enum
+public enum EasySingleton{
+    INSTANCE;
+}
+/*
+可以通过EasySingleton.INSTANCE来访问实例，这比调用getInstance()方法简单多了。创建枚举默认就是线程安全的，所以不需要担心double checked locking，而且还能防止反序列化导致重新创建新的对象。
+*/
